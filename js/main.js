224f34fb89473058230bfc4afdecacea2c860047
Behruz-01 <bekhruzgaybullaev@gmail.com>
2022-02-21 12:18:39 +0500

    initialy commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var a =Number(500);
+var b =Number(250);
+var x =Number(120);
+
+a=a*10.900;
+b=b*10.900;
+x=x*12272,64;
+
+var collected=Number((a+b+x));
+
+console.log(collected);
+
+
+var value = prompt("mablag'ingizni kiriting" , "so'mda");
+
+if(value>=collected){
+    console.log("oq yo'l oka");
+}else{
+    console.log("keyingi safar");
+}
